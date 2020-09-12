376bb65e87102b9d42156f1869a7f43f0a989546
Hussam Hallak <hhallak@cs.odu.edu>
2020-09-12 17:23:33 -0400

    refactor into a module

--- gts.py
+++ gts.py
@@ -7,65 +7,47 @@ from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 import os
 import sys
-import requests
 
-if len(sys.argv) != 2:
-    print ("Usage: Python gts.py <url>")
-    print ("e.g: python gts.py http://example.com")
-    sys.exit()
-else:
-    url = sys.argv[1]
-    request = requests.get(url)
-    if request.status_code != 200:
-        print ("This URL did not return a status code of 200. Try a different URL.")
-        print ("Status Code:" + str(request.status_code))
-        exit()
-
-def printArray(array, array_name):
-    print (array_name + ":" + "\n" + "--------------")
-    for item in array:
-        print (item)
-        ar_item = translator.translate(item, src='en', dest='ar')
-        print (ar_item.text)
-
-st = StanfordNERTagger(r'stanford-ner-4.0.0/classifiers/english.all.3class.distsim.crf.ser.gz',
-					   r'stanford-ner-4.0.0/stanford-ner.jar',
-					   encoding='utf-8')
-
+st = StanfordNERTagger(r'stanford-ner-4.0.0/classifiers/english.all.3class.distsim.crf.ser.gz', r'stanford-ner-4.0.0/stanford-ner.jar', encoding='utf-8')
+translator = Translator()
 extractor = extractors.ArticleExtractor()
 
-# From a URL
-content = extractor.get_content_from_url(url)
+TRACKED_CLASSES = ["LOCATION", "PERSON", "ORGANIZATION"]
 
-#print (content)
+def extract(url):
 
-translator = Translator()
+	try:
+		content = extractor.get_content_from_url(url)
+	except:
+		print ("This URL did not return a status code of 200. Try a different URL.")
+		return
 
-output = translator.translate(content)
+	output = translator.translate(content)
 
-translated_content = output.text
+	translated_content = output.text
 
-#print (translated_content)
+	tokenized_text = word_tokenize(translated_content)
 
-tokenized_text = word_tokenize(translated_content)
+	tagged_text = st.tag(tokenized_text)
 
-classified_text = st.tag(tokenized_text)
+	res = {k: [] for k in TRACKED_CLASSES}
+	for item in tagged_text:
+		if item[1] in TRACKED_CLASSES:
+			ar_item = translator.translate(item[0], src='en', dest='ar')
+			res[item[1]].append((item[0], ar_item.text))
+	return res
 
-#print(type(classified_text))
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print ("Usage: Python gts.py <url>")
+		print ("e.g: python gts.py http://example.com")
+		sys.exit()
+	else:
+		ext_entities = extract(sys.argv[1])
+		if not ext_entities:
+			sys.exit()
+		for cat in TRACKED_CLASSES:
+			print("----------- " + cat + " -------------")
+			for e in ext_entities[cat]:
+				print(e[0], e[1])
 
-locations = []
-persons = []
-organizations = []
-
-for word in classified_text:
-    if word[1] == "LOCATION":
-        locations.append(word[0])
-    if word[1] == "PERSON":
-        persons.append(word[0])
-    if word[1] == "ORGANIZATION":
-        organizations.append(word[0])
-
-
-printArray(locations, "Locations")
-printArray(organizations, "Organizations")
-printArray(persons, "Persons")
